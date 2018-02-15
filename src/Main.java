80ddae5963dbc568a426d861bdc9cf06dddafbb7
Jaanus <jakell@ttu.ee>
2018-02-15 22:00:04 +0200

    make GET and POST request to localhost:8000/test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.URL;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -27,6 +26,60 @@ public class Main {
         server.createContext("/test", new MyHandler());
         server.setExecutor(null);
         server.start();
+
+        sendGetRequest();
+        sendPostRequest();
+    }
+
+    private static void sendGetRequest() throws IOException {
+        URL url = new URL("http://localhost:8000/test");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = connection.getResponseCode();
+        System.out.println("GET ResponseCode " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            InputStream is = connection.getInputStream();
+            byte[] dataBytes = inputStream2ByteArray(is);
+            String data = new String(dataBytes);
+            System.out.println("RESPONSE " + data);
+        }
+    }
+
+    private static void sendPostRequest() throws IOException {
+        URL url = new URL("http://localhost:8000/test");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+
+        connection.setDoOutput(true);
+        OutputStream os = connection.getOutputStream();
+        os.write("{\"data\": \"value\"}".getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("POST ResponseCode " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            InputStream is = connection.getInputStream();
+            byte[] dataBytes = inputStream2ByteArray(is);
+            String data = new String(dataBytes);
+            System.out.println("RESPONSE " + data);
+        }
+    }
+
+    private static byte[] inputStream2ByteArray(InputStream is) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        int nRead;
+        byte[] data = new byte[16384];
+
+        while ((nRead = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        buffer.flush();
+
+        return buffer.toByteArray();
     }
 
     static class MyHandler implements HttpHandler {
@@ -34,27 +87,14 @@ public class Main {
         private void handleRequest(HttpExchange t) throws IOException {
             System.out.println(t.getRequestURI().toASCIIString());
             System.out.println(t.getRequestMethod());
+            System.out.println(t.getRemoteAddress().getAddress().getHostAddress());
+            System.out.println(t.getRemoteAddress().getPort());
             for (String header: t.getRequestHeaders().keySet()) {
                 System.out.println(header + ": " + t.getRequestHeaders().get(header));
             }
             InputStream is = t.getRequestBody();
             String data = new String(inputStream2ByteArray(is));
-            System.out.println(data);
-        }
-
-        private byte[] inputStream2ByteArray(InputStream is) throws IOException {
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-            int nRead;
-            byte[] data = new byte[16384];
-
-            while ((nRead = is.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-
-            buffer.flush();
-
-            return buffer.toByteArray();
+            System.out.println("DATA " + data);
         }
 
         private void handleResponse(HttpExchange t) throws IOException {
